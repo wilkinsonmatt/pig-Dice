49b99d05acb9fec2fb0022cbf3e6394f0f886bcd
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-15 11:17:54 -0700

    create game, player constructor, addPlayers method

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,36 @@
-function Game() {
-  this.players = {};
-  this.numberOfPlayers = 0;
-  this.turn = turn;
+function Game(numberofPlayers) {
+  this.players = [];
+  this.numberOfPlayers = numberofPlayers;
+  this.turn = 0;
 }
 
-function Player() {
+let currentgame;
+
+Game.prototype.AddPlayers = function () {
+  console.log("hi");
+  for (let i = 0; i <= currentgame.numberOfPlayers; i++) {
+    let newplayer = new Player(i);
+    currentgame.players.push(newplayer);
+  }
+};
+
+function Player(playerid) {
   this.totalscore = 0;
-  this.tempscore = 0;
-  this.playerNumber = playerNumber;
+  this.tempscore = 45;
+  this.playerNumber = playerid;
 }
+
 
 $(document).ready(function () {
   $("form#pigForm").submit(function (event) {
     event.preventDefault();
-    const flavor = $("input:radio[name=flavor]:checked").val();
+    const numberofPlayers = parseInt(
+      $("input:radio[name=player]:checked").val()
+    );
+    currentgame = new Game(numberofPlayers);
+    currentgame.AddPlayers();
+    console.log(currentgame.players[0].playerNumber);
+
+
   });
 });
