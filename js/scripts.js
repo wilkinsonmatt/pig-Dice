0ec71129361a5fd823d1104d0e75952ce1afe265
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-19 22:59:34 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,6 @@ Game.prototype.AddPlayers = function () {
     // console.log(i);
     let newplayer = new Player(i);
     currentgame.players.push(newplayer);
-    currentgame.players;
   }
 };
 
@@ -120,9 +119,7 @@ function hold(playerid) {
 $(document).ready(function () {
   $("form#pigForm").submit(function (event) {
     event.preventDefault();
-    const numberofPlayers = parseInt(
-      $("input:radio[name=player]:checked").val()
-    );
+    const numberofPlayers = parseInt($("input:radio[name=player]:checked").val());
     currentgame = new Game(numberofPlayers);
     currentgame.AddPlayers();
     $(".start-menu").hide();
